9dcaca4a0f02ba2376fae8d760435f197c5ee093
Yash Mehta <yashmehta0395@gmail.com>
2023-10-02 19:37:01 +0530

    basic UI setup done for Navbar

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -1,44 +1,44 @@
-"use client"
+"use client";
 
-import React { createContext, useContext, useState, useEffect } from "react"
+import React, { createContext, useContext, useState, useEffect } from "react";
 
 interface ThemeContextType {
-    mode: string;
-    setMode:(mode: string) => void;
+	mode: string;
+	setMode: (mode: string) => void;
 }
 
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-    const [mode, setMode] = useState('');
+	const [mode, setMode] = useState("");
 
-    const handleThemeChange = () => {
-        if(mode === 'dark') {
-            setMode('light');
-            document.documentElement.classList.add('light');
-        } else {
-            setMode('dark');
-            document.documentElement.classList.add('light');
-        }
-    }
+	const handleThemeChange = () => {
+		if (mode === "dark") {
+			setMode("light");
+			document.documentElement.classList.add("light");
+		} else {
+			setMode("dark");
+			document.documentElement.classList.add("light");
+		}
+	};
 
-    useEffect(() => {
-        handleThemeChange();
-    }, [mode])
+	useEffect(() => {
+		handleThemeChange();
+	}, [mode]);
 
-    return {
-        <ThemeContext.Provider value={{ mode, SetMode }}>
-        {children}
-        </ThemeContext.Provider>
-    }
+	return (
+		<ThemeContext.Provider value={{ mode, setMode }}>
+			{children}
+		</ThemeContext.Provider>
+	);
 }
 
 export function useTheme() {
-    const context = useContext(ThemeContext);
+	const context = useContext(ThemeContext);
 
-    if(context === undefined) {
-        throw new Error('useTheme must be used within  a ThemeProvider')
-    }
+	if (context === undefined) {
+		throw new Error("useTheme must be used within  a ThemeProvider");
+	}
 
-    return context;
+	return context;
 }
