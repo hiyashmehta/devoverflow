7952f37964f7e38cacf6a26ba2e6bc3782413536
Yash Mehta <yashmehta0395@gmail.com>
2023-10-10 19:39:39 +0530

    Made the form reusable

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
 import { zodResolver } from "@hookform/resolvers/zod"
 
@@ -19,8 +19,11 @@ import { QuestionsSchema } from "@/lib/validations"
 import { Badge } from '../ui/badge';
 import Image from 'next/image';
 
+const type:any = 'create'
+
 const Question = () => {
     const editorRef = useRef(null);
+    const [isSubmitting, setIsSubmitting] = useState(false);
 
     const log = () => {
         if (editorRef.current) {
@@ -40,9 +43,18 @@ const Question = () => {
 
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof QuestionsSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+    setIsSubmitting(true);
+    
+    try {
+      // make an async call to your API -> to make a question
+      // contain all form data
+
+      // navigate to home page
+    } catch (error) {
+
+    } finally {
+      setIsSubmitting(false);
+    }
   }
 
     const handleInputKeyDown = (e: React.KeyboardEvent<HTMLInputElement>, field: any) => {
@@ -172,7 +184,17 @@ const Question = () => {
           </FormItem>
         )}
       />
-      <Button type="submit">Submit</Button>
+      <Button type="submit" className='primary-gradient w-fit !text-light-900' disabled={isSubmitting}>
+        {isSubmitting ? (
+          <>
+            {type === 'edit' ? 'Editing...' : 'Posting...'}
+          </>
+        ) : (
+            <>
+            {type === 'edit' ? 'Edit Question' : 'Ask a Question'}
+            </>
+        )}
+      </Button>
     </form>
   </Form>
   )
