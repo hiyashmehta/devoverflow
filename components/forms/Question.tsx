3073d61f85aab32140e2ff5cd39fca3a7cbc528a
Yash Mehta <yashmehta0395@gmail.com>
2023-10-11 11:21:02 +0530

    Succesfully connected project with database

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -18,6 +18,8 @@ import { Button } from "../ui/button"
 import { QuestionsSchema } from "@/lib/validations"
 import { Badge } from '../ui/badge';
 import Image from 'next/image';
+import { createQuestion } from '@/lib/actions/question.action';
+import { z } from 'zod';
 
 const type:any = 'create'
 
@@ -42,12 +44,14 @@ const Question = () => {
   })
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof QuestionsSchema>) {
+  async function onSubmit(values: z.infer<typeof QuestionsSchema>) {
     setIsSubmitting(true);
     
     try {
       // make an async call to your API -> to make a question
       // contain all form data
+
+      await createQuestion({})
 
       // navigate to home page
     } catch (error) {
@@ -122,6 +126,8 @@ const Question = () => {
                 onInit={(evt, editor) =>{ 
                     // @ts-ignore
                     editorRef.current = editor}}
+                onBlur={field.onBlur}
+                onEditorChange={(content) => field.onChange(content)}
                 initialValue=""
                 init={{
                     height: 350,
