100fc0424cf2e0001ee5a7a25ee497c15fc6747d
Yash Mehta <yashmehta0395@gmail.com>
2023-10-14 15:45:16 +0530

    .

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -10,66 +10,67 @@ import Link from "next/link";
 
 export default async function Home() {
 	const result = await getQuestions({});
+	if (result)
+		return (
+			<>
+				<div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+					<h1 className="h1-bold text-dark100_light900">
+						All Questions
+					</h1>
 
-	return (
-		<>
-			<div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
-				<h1 className="h1-bold text-dark100_light900">All Questions</h1>
+					<Link
+						href="/ask-question"
+						className="flex justify-end max-sm:w-full"
+					>
+						<Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+							Ask a Question
+						</Button>
+					</Link>
+				</div>
 
-				<Link
-					href="/ask-question"
-					className="flex justify-end max-sm:w-full"
-				>
-					<Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
-						Ask a Question
-					</Button>
-				</Link>
-			</div>
-
-			<div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
-				<LocalSearchbar
-					route="/"
-					iconPosition="right"
-					imgSrc="/assets/icons/search.svg"
-					placeholder="Search for questions"
-					otherClasses="flex-1"
-				/>
-
-				<Filter
-					filters={HomePageFilters}
-					otherClasses="min-h-[56px] sm:min-w-[170px]"
-					containerClasses="hidden max-md:flex"
-				/>
-			</div>
-
-			<HomeFilters />
-
-			<div className="mt-10 flex w-full flex-col gap-6">
-				{result.questions.length > 0 ? (
-					result.questions.map((question) => (
-						<QuestionCard
-							key={question._id}
-							_id={question._id}
-							title={question.title}
-							tags={question.tags}
-							author={question.author}
-							upvotes={question.upvotes}
-							views={question.views}
-							answers={question.answers}
-							createdAt={question.createdAt}
-						/>
-					))
-				) : (
-					<NoResult
-						title="There is no question to show"
-						description="Be the first to break the silence!  Ask a question and kickstart the discussion. Our query could be the next big thing others learn from. Get involved"
-						link="/ask-question"
-						linkTitle="Ask a Question"
+				<div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+					<LocalSearchbar
+						route="/"
+						iconPosition="right"
+						imgSrc="/assets/icons/search.svg"
+						placeholder="Search for questions"
+						otherClasses="flex-1"
 					/>
-				)}
-			</div>
-		</>
-	);
+
+					<Filter
+						filters={HomePageFilters}
+						otherClasses="min-h-[56px] sm:min-w-[170px]"
+						containerClasses="hidden max-md:flex"
+					/>
+				</div>
+
+				<HomeFilters />
+
+				<div className="mt-10 flex w-full flex-col gap-6">
+					{result.questions.length > 0 ? (
+						result.questions.map((question) => (
+							<QuestionCard
+								key={question._id}
+								_id={question._id}
+								title={question.title}
+								tags={question.tags}
+								author={question.author}
+								upvotes={question.upvotes}
+								views={question.views}
+								answers={question.answers}
+								createdAt={question.createdAt}
+							/>
+						))
+					) : (
+						<NoResult
+							title="There is no question to show"
+							description="Be the first to break the silence!  Ask a question and kickstart the discussion. Our query could be the next big thing others learn from. Get involved"
+							link="/ask-question"
+							linkTitle="Ask a Question"
+						/>
+					)}
+				</div>
+			</>
+		);
 }
 
-export const runtime = "edge";
