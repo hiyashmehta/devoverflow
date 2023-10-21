528d154c9b4fc2ba3fb4cd953bc008652dae699a
Yash Mehta <yashmehta0395@gmail.com>
2023-10-21 13:14:46 +0530

    Question Details View created successfully

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { downvoteAnswer, upvoteAnswer } from "@/lib/actions/answer.action";
+import { viewQuestion } from "@/lib/actions/interaction.action";
 import { downvoteQuestion, upvoteQuestion } from "@/lib/actions/question.action";
 import { toggleSaveQuestion } from "@/lib/actions/user.action";
 import { formatAndDivideNumber } from "@/lib/utils";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
-import React from "react";
+import { useEffect } from "react";
 
 interface Props {
 	type: string;
@@ -29,7 +30,7 @@ const Votes = ({
 	hasSaved,
 }: Props) => {
     const pathName = usePathname();
-    // const router = useRouter();
+ 	const router = useRouter();
 
 	const handleSave = async () => {
 		await toggleSaveQuestion({
@@ -90,6 +91,13 @@ const Votes = ({
             return;
         }
     }
+
+	useEffect(() => {
+		viewQuestion({
+			questionId: JSON.parse(itemId),
+			userId: userId ? JSON.parse(userId) : undefined,
+		});
+	}, [ itemId, userId, pathName, router ]);
 
 	return (
 		<div className="flex gap-5">
---
+++ lib/actions/interaction.action.ts
@@ -0,0 +1,38 @@
+"use server";
+
+import Question from "@/database/question.model";
+import { connectToDatabase } from "../mongoose";
+import { ViewQuestionParams } from "./shared.types";
+import Interaction from "@/database/interaction.model";
+
+
+export async function viewQuestion(params: ViewQuestionParams) {
+	try {
+		await connectToDatabase();
+
+        const { questionId, userId } = params;
+
+        // update view count for the question
+        await Question.findByIdAndUpdate(questionId, { $inc : { views: 1 } });
+
+        if(userId) {
+            const existingInteraction = await Interaction.findOne({
+                user: userId,
+                question: questionId,
+                action: 'view'
+            });
+
+            if(existingInteraction) return console.log('User has already viewed this question');
+
+            // Create Interaction
+            await Interaction.create({
+                user: userId,
+                question: questionId,
+                action: 'view'
+            });
+        }
+	} catch (error) {
+		console.log(error);
+		throw error;
+	}
+ }
