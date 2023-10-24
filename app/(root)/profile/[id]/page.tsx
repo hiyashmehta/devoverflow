ea4c9b39919b7d03a31b7d84772ab9900fb442d5
Yash Mehta <yashmehta0395@gmail.com>
2023-10-24 22:51:46 +0530

    Created User Stats UI Successfully

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -9,6 +9,7 @@ import Link from "next/link";
 import React from "react";
 import { getJoinedDate } from "@/lib/utils";
 import ProfileLink from "@/components/shared/ProfileLink";
+import Stats from "@/components/shared/Stats";
 
 // import Stats from "@/components/profile/Stats";
 
@@ -78,7 +79,10 @@ const Page = async ({ params, searchParams }: URLProps) => {
 				</div>
 			</div>
 
-			{/* <Stats /> */}
+			<Stats 
+                totalQuestions={userInfo.totalQuestions}
+                totalAnswers={userInfo.totalAnswers}
+            />
 
 			<div className="mt-10 flex gap-10">
 				<Tabs defaultValue="top-posts" className="flex-1">
