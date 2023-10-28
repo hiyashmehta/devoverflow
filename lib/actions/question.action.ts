363daa23fa50e2bec704ab523b2e3cf5687c1e1d
Yash Mehta <yashmehta0395@gmail.com>
2023-10-28 20:07:19 +0530

    Show Top Questions function added successfully

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -211,3 +211,18 @@ export async function editQuestion(params: EditQuestionParams) {
 		throw error;
 	}
 }
+
+export async function getHotQuestions() {
+	try{
+		await connectToDatabase();
+
+		const hotQuestions = await Question.find({})
+		.sort({ views: -1, upvotes: -1 })
+		.limit(5)
+
+		return { hotQuestions };
+	} catch(error) {
+		console.log(error);
+		throw error;
+	}
+}
