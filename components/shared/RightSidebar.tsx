d318b4cf0a5b98fdd46b66caf288cf3aa9263e7a
Yash Mehta <yashmehta0395@gmail.com>
2023-10-28 20:49:21 +0530

    Successfully implemented Show Top Tags

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -3,18 +3,11 @@ import Link from 'next/link';
 import React from 'react'
 import RenderTag from './RenderTag';
 import { getHotQuestions } from '@/lib/actions/question.action';
-
-const popularTags = [
-    { _id: '1', name: 'javascript', totalQuestions: 5 },
-    { _id: '2', name: 'react', totalQuestions: 5 },
-    { _id: '3', name: 'next', totalQuestions: 5 },
-    { _id: '4', name: 'vue', totalQuestions: 2 },
-    { _id: '5', name: 'redux', totalQuestions: 10 }
-]
-
+import { getTopPopularTags } from '@/lib/actions/tag.actions';
 
 const RightSidebar = async () => {
     const hotQuestions = await getHotQuestions();
+    const popularTags = await getTopPopularTags();
 
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
@@ -47,7 +40,7 @@ const RightSidebar = async () => {
             key={tag._id}
             _id={tag._id}
             name={tag.name}
-            totalQuestions={tag.totalQuestions}
+            totalQuestions={tag.numberOfQuestions}
             showCount
             />
            ))}
