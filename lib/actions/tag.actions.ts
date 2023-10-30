3b3a8625c5c8690fb2e02d3b92f9b39b7667f46c
Yash Mehta <yashmehta0395@gmail.com>
2023-10-30 11:25:46 +0530

    Integrated Filters on Tags Page Successfully

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -38,7 +38,7 @@ export async function getAllTags(params: GetAllTagsParams) {
 	try {
 		await connectToDatabase();
 
-		const { searchQuery } = params;
+		const { searchQuery, filter } = params;
 
 		const query: _FilterQuery<typeof Tag> ={};
 
@@ -46,7 +46,26 @@ export async function getAllTags(params: GetAllTagsParams) {
 			query.$or = [{name: {$regex: new RegExp(searchQuery, "i")}}]
 		}
 
-		const tags = await Tag.find(query);
+		let sortOptions = {};
+
+		switch(filter){
+			case "popular":
+				sortOptions = { question: 1 }
+			break;
+			case "recent":
+				sortOptions = { createdAt: -1 }
+	   		break;
+		case "name":
+			sortOptions = { name: 1 }
+		break;
+		case "old":
+			sortOptions = { createdAt: 1 }
+		break;
+		default:
+		break;
+		}
+		const tags = await Tag.find(query)
+		.sort(sortOptions)
 
 		return { tags };
 	} catch (error) {
