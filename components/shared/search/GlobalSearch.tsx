2f8fc34b22de7356fc76afc53690abad2c816198
Yash Mehta <yashmehta0395@gmail.com>
2023-11-01 20:22:32 +0530

    Successfully Created  the GlobalSearch UI

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,9 +1,46 @@
+"use client"
+
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Input } from "@/components/ui/input"
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { formUrlQuery, removeKeysFromQuery } from '@/lib/utils'
+import GlobalResult from './GlobalResult'
 
 
 const GlobalSearch = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const query = searchParams.get('global');
+  const [ search, setSearch ] = useState(query || '');
+  const [ isOpen, setIsOpen ] = useState(false)
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(()=> {
+      if(search) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: 'global',
+          value: search
+        })
+
+        router.push(newUrl, { scroll: false});
+      } else {
+        if(query){
+          const newUrl = removeKeysFromQuery({
+            params: searchParams.toString(),
+            keysToRemove: ['global', 'type']
+          })
+
+          router.push(newUrl, { scroll: false });
+        }
+      }
+    }, 300);
+    return () => clearTimeout(delayDebounceFn)
+  }, [search, pathname, router, searchParams, query])
+
   return (
     <div className='relative w-full max-w-[600px] max-lg:hidden'>
         <div className='background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-4 rounded-xl px-4'>
@@ -16,11 +53,18 @@ const GlobalSearch = () => {
             />
             <Input
                 type="text"
+                value={search}
+                onChange={(e) => {
+                  setSearch(e.target.value);
+
+                  if(!isOpen) setIsOpen(true);
+                  if(!e.target.value) setIsOpen(false);
+                }}
                 placeholder='Search globally'
-                value=""
-                className='paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none'
+                className='paragraph-regular no-focus placeholder background-light800_darkgradient text-dark400_light700 border-none shadow-none outline-none'
             />
         </div>
+        { isOpen && <GlobalResult /> }
     </div>
   )
 }
