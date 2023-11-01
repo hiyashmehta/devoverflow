a5b95d7cd283013e5a6a6d869c6ca8b7683e7280
Yash Mehta <yashmehta0395@gmail.com>
2023-11-01 11:14:04 +0530

    Implemented Pagination on Community Page

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -92,7 +92,9 @@ export async function getAllUsers(params: GetAllUsersParams) {
 	try {
 		await connectToDatabase();
 
-		const { searchQuery, filter } = params;
+		const { searchQuery, filter, page = 1, pageSize = 10 } = params;
+
+		const skipAmount = (page - 1) * pageSize;
 
 		const query: FilterQuery<typeof User> = {};
 
@@ -120,9 +122,13 @@ export async function getAllUsers(params: GetAllUsersParams) {
 		}
 
 		const users = await User.find(query)
+		.skip(skipAmount)
+		.limit(pageSize)
 		.sort(sortOptions);
 
-		return { users };
+		const totalUsers = await User.countDocuments(query);
+		const isNext = totalUsers > skipAmount+users.length;
+		return { users, isNext };
 	} catch (error) {
 		console.log(error);
 		throw error;
